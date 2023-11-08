e2232fc8381c1e12bcdc2f58776092bd520478ca
rishabhDev2700 <rishavkumar2700@gmail.com>
2023-11-08 20:58:42 +0530

    Final  draft

--- script.js
+++ script.js
@@ -32,5 +32,5 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 8000); // Change image every 2 seconds
+  setTimeout(showSlides, 8000);
 }
